011883280e57bfe50e490074b08dfc4cc8b6468b
JunixOs <yonelsilva75@gmail.com>
2026-06-03 11:43:58 -0500

    feat(api, infrastructure, application): Añade logica para insercion de datos de auditoria y caching.

--- src/ZooTech.API/Program.cs
+++ src/ZooTech.API/Program.cs
@@ -122,6 +122,9 @@ builder.Services.AddScoped<
     ICreateTenantOutputPort>(
         sp => sp.GetRequiredService<
             CreateTenantPresenter>());
+
+builder.Services.AddScoped<ITenantDatabaseMigrator , TenantDatabaseMigrator>();
+builder.Services.AddScoped<IGanaderiaDbContextFactory , GanaderiaDbContextFactory>();
 // ======= Configuracion DI =======
 
 var frontendPort = builder.Configuration["Frontend:FrontendPort"];
---
+++ src/ZooTech.Infrastructure/Caching/RedisCacheService.cs
@@ -0,0 +1,73 @@
+using System.Globalization;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using ZooTech.Application.Common.Gateway.Caching;
+
+namespace ZooTech.Infrastructure.Caching
+{
+    public class RedisCacheService : IAppCacheService
+    {
+        private readonly RedisCacheConnection _redisCacheConnection;
+        private readonly TimeSpan _expirationTimeSpan;
+
+        public RedisCacheService(
+            RedisCacheConnection redisCacheConnection , 
+            IConfiguration configuration
+        )
+        {
+            _redisCacheConnection = redisCacheConnection;
+            
+            var expirationTime = configuration["Redis:ExpirationTime"];
+            if(TimeSpan.TryParseExact(
+                expirationTime,
+                @"mm\:ss",
+                CultureInfo.InvariantCulture,
+                out var timeSpan))
+            {
+                _expirationTimeSpan = new TimeSpan(0, timeSpan.Minutes , timeSpan.Seconds);
+            }
+            else
+            {
+                _expirationTimeSpan = new TimeSpan(0, 3 , 0);
+            }
+
+            
+        }
+
+        public async Task<T> GetOrCreateAsync<T>(
+            string key, 
+            Func<Task<T>> factory
+        )
+        {
+            var redisDatabase = _redisCacheConnection.GetDatabase();
+
+            var cachedValue = await redisDatabase.StringGetAsync(key);
+
+            if (cachedValue.HasValue)
+            {
+                byte[] bytes = cachedValue!;
+
+                var cachedResult =  JsonSerializer.Deserialize<T>(
+                    bytes
+                )!;
+
+                if (cachedResult is not null)
+                {
+                    return cachedResult;
+                }
+            }
+
+            var result = await factory();
+
+            var serialized = JsonSerializer.Serialize(result);
+
+            await redisDatabase.StringSetAsync(
+                key ,
+                serialized , 
+                _expirationTimeSpan
+            );
+
+            return result;
+        }
+    }
+}
