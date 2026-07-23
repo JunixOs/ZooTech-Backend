91935d76ec7612f5e06bde4fa78de85b6a3a7855
KaliJhomen <a1d4r1310@gmail.com>
2026-07-23 00:32:53 -0500

    PROGRAM Y APPSETTINGS COPIADOS A COMO ESTABAN EN DEVELOP 23/07

--- src/ZooTech.API/Program.cs
+++ src/ZooTech.API/Program.cs
@@ -108,17 +108,12 @@ builder.Services.AddCors(options =>
                     uri.Scheme == "http" &&
                     uri.Port == 4200 &&
                     (
-                        uri.Host == "admin.zentrycorp.local" ||
-                        uri.Host == "zootecniaunas.zentrycorp.local" ||
-                        uri.Host == "elroble.zentrycorp.local" ||
-                        uri.Host == "lacteosdelvalle.zentrycorp.local" ||
-                        uri.Host == "losandes.zentrycorp.local" ||
                         uri.Host == "localhost" ||
-                        uri.Host == "127.0.0.1"
+                        uri.Host == "127.0.0.1" ||
+                        uri.Host.EndsWith(".zentrycorp.local")
                     );
 
                 return isZentryDomain || isLocal;
-
             })
             .AllowAnyHeader()
             .AllowAnyMethod()
